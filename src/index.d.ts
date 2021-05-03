5d552c7b8626c945285d0c9fdb64008dfca7eb06
3axap4eHko <3axap4eHko@gmail.com>
2021-05-02 22:28:30 -0400

    ConfigSchema typing

--- src/index.d.ts
+++ src/index.d.ts
@@ -11,4 +11,26 @@ export interface Ascertain {
     (data: any): void;
 }
 
-export default function (schema: Schema, rootName?: string): Ascertain;
+type SchemaType = any;
+
+export type ConfigSchema<T extends Record<string, unknown>> = {
+  [L1 in keyof T]?: T[L1] extends Record<string, unknown>
+  ? {
+    [L2 in keyof T[L1]]?: T[L1][L2] extends Record<string, unknown>
+    ? {
+      [L3 in keyof T[L1][L2]]?: T[L1][L2][L3] extends Record<string, unknown>
+      ? {
+        [L4 in keyof T[L1][L2][L3]]?: T[L1][L2][L3][L4] extends Record<string, unknown>
+        ? {
+          [L5 in keyof T[L1][L2][L3][L4]]?: SchemaType
+        }
+        : SchemaType
+      }
+      : SchemaType
+    }
+    : SchemaType
+  }
+  : null
+}
+
+export default function <T extends Record<string | number, unknown>>(schema: ConfigSchema<T>, rootName?: string): Ascertain;
